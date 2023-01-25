b009b5aff81973005151a2f3888c0059a0ce486c
dylanphe <dylanphe@g.ucla.edu>
2023-01-24 23:10:09 -0800

    added content-length and content-type headers resolver

--- client.c
+++ client.c
@@ -6,13 +6,13 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 15635
+#define PORT 8080
 
 int main(int argc, char const *argv[]) {
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
   char *hello =
-      "GET /index.html HTTP/1.1\r\n\r\n";
+      "GET /ind1ex.html HTTP/1.1\r\n\r\n";
   char buffer[1024] = {0};
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
   send(sock, hello, strlen(hello), 0);
+
   valread = read(sock, buffer, 1024);
   printf("%s\n", buffer);
-  return 0;
 }
--- server.c
+++ server.c
@@ -5,22 +5,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define PORT 15635
+#define PORT 8080
 #define BACKLOG 5
 #define MAX_REQ_LEN 1024
+#define MAX_BLOCK_SIZE 1024
 
-
+#define DEFAULT_EXT "Content-Type: application/octet-stream"
 
 void handle_connection(int cli_socket) {
     char buffer[MAX_REQ_LEN];
+    char block_buf[MAX_BLOCK_SIZE];
+    char *file_ext;
     char *method;
     char *url;
     char *httpvers;
     char *header, *value;
+    char *contenttype = DEFAULT_EXT;
+    char contentlen[32] = "Content-Length: ";
 
     int valread = read(cli_socket, buffer, 1024);
 
@@ -38,18 +44,60 @@ void handle_connection(int cli_socket) {
     printf("%s: %s\n", header, value);*/
 
     int file_fd = open(url + 1, O_RDONLY, S_IRUSR);
-    
     if (file_fd == -1) {
-        char *res = "HTTP/1.0 404 Not Found\r\n\r\n";
-        write(cli_socket, res, 100);
+        char *res = "HTTP/1.1 404 NOT FOUND\r\n";
+        write(cli_socket, res, strlen(res));
+        write(cli_socket, "\r\n", 4);
         perror("webserver: Closing connection, file not found");
         close(cli_socket);
+        exit(EXIT_FAILURE);
     }
-    write(cli_socket, "HTTP/1.0 200 OK\r\n\r\n", 100);
+
+    //Checking file size to set content-len
+    struct stat file_st;
+    long file_size;
+    if (stat(url+1, &file_st) == 0) {
+        file_size = file_st.st_size;
+        char len[sizeof(long)*8+1];
+        sprintf(len, "%ld", file_size);
+        strcat(contentlen, len);
+        strcat(contentlen, "\r\n");
+    }
+
+    //Checking file extension to set content-type
+    file_ext = strrchr(url, '.');
+    if (strcmp(file_ext, ".html") == 0 || strcmp(file_ext, ".htm") == 0) {
+        contenttype = "Content-Type: text/html\r\n";
+    }
+    else if (strcmp(file_ext, "txt") == 0) { 
+        contenttype = "Content-Type: text/plain\r\n"; 
+    }
+    else if (strcmp(file_ext, ".jpeg") == 0 || strcmp(file_ext, ".jpg") == 0) {
+        contenttype = "Content-Type: image/jpg\r\n"; 
+    }
+    else if (strcmp(file_ext, ".png") == 0) { 
+        contenttype = "Content-Type: image/png\r\n"; 
+    }
+    else if (strcmp(file_ext, ".pdf") == 0) { 
+        contenttype = "Content-Type: application/pdf\r\n"; 
+    }
+
+    // TO DO: if file exist, save the content to buffer
+    char file_buf[file_size * sizeof(char)];
+    memset(file_buf, 0, file_size * sizeof(char));
+    if (read(file_fd, file_buf, file_size) < 0){
+		perror("webserver: Read error.");
+	}
+
+    //Send response message
+    char *res = "HTTP/1.1 200 OK\r\n";
+    write(cli_socket, res, strlen(res));
+    write(cli_socket, contentlen, strlen(contentlen));
+    write(cli_socket, contenttype, strlen(contenttype));
+    write(cli_socket, "\r\n", 4);
+    write(cli_socket, file_buf, strlen(file_buf));
+    close(file_fd);
     close(cli_socket);
-
-
-
 }
 
 int main(int argc, char const *argv[]) {
@@ -64,19 +112,18 @@ int main(int argc, char const *argv[]) {
 
     // Initialize socket address info 
     // htons: func to convert int from host byte order to network byte order
-    // READ MORE: https://man7.org/linux/man-pages/man7/ip.7.html#:~:text=sin_addr%20is%20the%20IP%20host,address%20in%20network%20byte%20order.        // INADDR_ANY - allows socket to be binded to any IP addresses.
+    // READ MORE: https://man7.org/linux/man-pages/man7/ip.7.html#:~:text=sin_addr%20is%20the%20IP%20host,address%20in%20network%20byte%20order.        
+    // INADDR_ANY - allows socket to be binded to any IP addresses.
     struct sockaddr_in serv_addr, cli_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
     int addrlen = sizeof(serv_addr);
     int opt = 1;
-
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
         perror("webserver: setsockopt() failure error.");
         exit(EXIT_FAILURE);
     }
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
 
     // Bind the server socket to a local IP address and port number
     if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
@@ -104,6 +151,7 @@ int main(int argc, char const *argv[]) {
         inet_ntop(AF_INET, &(cli_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
         printf("webserver: connected to client: %s\n", client_ip);
 
+        //call handle_connection to handle requests from connected clients
         handle_connection(newsock_fd);
     }
 }
